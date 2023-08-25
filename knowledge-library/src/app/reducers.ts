f065d2c2e063f259372cf8e6faf3cc81363e7488
Dávid Csejtei <csejteidavid90@gmail.com>
2023-08-25 11:18:48 +0200

    Clean up

--- knowledge-library/src/app/reducers.ts
+++ knowledge-library/src/app/reducers.ts
@@ -17,7 +17,7 @@ export function localStorageSyncReducer(
   reducer: ActionReducer<AppState>
 ): ActionReducer<AppState> {
   return localStorageSync({
-    keys: ['user'],
+    keys: ['user', 'categories'],
     rehydrate: true,
   })(reducer);
 }
